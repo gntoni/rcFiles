e3cf95febeb8699a2f74e8b7ca43b30ca2334807
Toni Gabas <toni2332@gmail.com>
2025-03-27 23:18:17 +0900

    add config to keep ssh agent

--- .system_config.bash
+++ .system_config.bash
@@ -16,3 +16,10 @@ scm_ps1() {
 PS1="\[\033[1;30m\]\u\[\033[1;36m\]@\[\033[1;36m\]\h:\[\033[01;36m\]\w\[\033[1;30m\] \$(scm_ps1)\n\$\[\033[00m\] " 
 LS_COLORS="tw=94:ow=94"; export LS_COLORS
 
+
+# SSH auth sock handling so that new SSH agents created by subsequent logons are still usable
+# https://gist.github.com/admackin/4507371
+_ssh_auth_save() {
+    ln -sf "$SSH_AUTH_SOCK" "$HOME/.ssh/ssh-auth-sock.$HOSTNAME"
+}
+alias tmux='_ssh_auth_save ; export HOSTNAME=$(hostname) ; tmux'
